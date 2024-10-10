3bc9225ce571a3e8e33fa3f58ea8c031c6c3ef14
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-09 22:38:41 -0700

    first commit

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState } from 'react';
+import ProjectHeader from '@/app/projects/ProjectHeader';
+import Board from '../BoardView';
+
+type Props = {
+    params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  return (
+    <div>
+      {/* MODAL NEW TASKS */}
+        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        { activeTab === "Board" && (
+          <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+        )}
+
+    </div>
+  )
+}
+
+export default Project
