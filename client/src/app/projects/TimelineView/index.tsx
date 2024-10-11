dabd08d25f4efad2c9a7a21d68e14d38abbaff67
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-11 03:49:58 -0700

    structuring timeline view with gantt

---
+++ client/src/app/projects/TimelineView/index.tsx
@@ -0,0 +1,29 @@
+import { useAppSelector } from "@/app/redux";
+import { useGetTasksQuery } from "@/state/api";
+import { DisplayOption, ViewMode } from "gantt-task-react";
+import React, { useState } from "react";
+
+type Props = {
+  id: string;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
+
+type TaskTypeItems = "task" | "milestone" | "project";
+
+const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const {
+    data: tasks,
+    error,
+    isLoading,
+  } = useGetTasksQuery({ projectId: Number(id) });
+
+  const [displayOptions, setDisplayOptions] = useState<DisplayOption>({
+    viewMode: ViewMode.Month,
+    locale: "en-US",
+  });
+
+  return <div>Timeline</div>;
+};
+
+export default Timeline;
