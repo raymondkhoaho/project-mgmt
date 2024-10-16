5a4b4932891cfa86e48b5524e82ca55f0fe7e4d9
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-16 05:02:36 -0700

    users page

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -70,7 +70,7 @@ export interface SearchResults {
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: ["Projects", "Tasks"],
+  tagTypes: ["Projects", "Tasks", "Users"],
   endpoints: (build) => ({
     getProjects: build.query<Project[], void>({
       query: () => "projects",
@@ -109,6 +109,10 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
+    getUsers: build.query<User[], void>({
+      query: () => "users",
+      providesTags: ["Users"]
+    }),
     search: build.query<SearchResults, string>({
       query: (query) => `search?query=${query}`,
     }), 
@@ -122,4 +126,5 @@ export const {
   useCreateTaskMutation,
   useUpdateTaskStatusMutation,
   useSearchQuery,
+  useGetUsersQuery,
 } = api;
