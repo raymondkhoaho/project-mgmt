a7e4d9442134f079475e3a321181846fb2b0d5d1
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-16 04:56:46 -0700

    user routes backend

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -13,7 +13,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
 const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
 const searchRoutes_1 = __importDefault(require("./routes/searchRoutes"));
-// import userRoutes from "./routes/userRoutes";
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 // import teamRoutes from "./routes/teamRoutes";
 /* CONFIGURATIONS */
 dotenv_1.default.config();
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 app.use("/projects", projectRoutes_1.default);
 app.use("/tasks", taskRoutes_1.default);
 app.use("/search", searchRoutes_1.default);
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes_1.default);
 // app.use("/teams", teamRoutes);
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import morgan from "morgan";
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
-// import userRoutes from "./routes/userRoutes";
+import userRoutes from "./routes/userRoutes";
 // import teamRoutes from "./routes/teamRoutes";
 
 /* CONFIGURATIONS */
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes);
 // app.use("/teams", teamRoutes);
 
 /* SERVER */
