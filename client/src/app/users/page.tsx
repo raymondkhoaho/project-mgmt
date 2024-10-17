35b419be891beb340ba61cfc31589918878eb257
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-17 03:17:13 -0700

    updated favicon and logo

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -29,7 +29,7 @@ const columns: GridColDef[] = [
     width: 100,
     renderCell: (params) => (
       <div className="flex h-full w-full items-center justify-center">
-        <div className="h-9 w-9">
+        <div className="h-12 w-12">
           <Image
             src={`/${params.value}`}
             alt={params.row.username}
