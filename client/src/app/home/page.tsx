abf90556bb8bb0d37e68ca45887504ff791d376b
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-23 18:22:35 -0700

    const variable priorityCount

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,5 +1,13 @@
-import { useGetTasksQuery } from "@/state/api";
+"use client";
+
+import {
+  useGetProjectsQuery,
+  useGetTasksQuery,
+  Priority,
+  Task,
+} from "@/state/api";
 import React from "react";
+import { useAppSelector } from "../redux";
 
 const HomePage = () => {
   const {
@@ -7,6 +15,23 @@ const HomePage = () => {
     isLoading: tasksLoading,
     isError: tasksError,
   } = useGetTasksQuery({ projectId: parseInt("1") });
+
+  const { data: projects, isLoading: isProjectsLoading } =
+    useGetProjectsQuery();
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  if (tasksLoading || isProjectsLoading) return <div>Loading...</div>;
+  if (tasksError || !tasks || !projects) return <div>Error fetching data</div>;
+
+  const priorityCount = tasks.reduce(
+    (acc: Record<string, number>, task: Task) => {
+      const { priority } = task;
+      acc[priority as Priority] = (acc[priority as Priority] || 0) + 1;
+      return acc;
+    },
+    {},
+  );
 
   return <div>HomePage</div>;
 };
