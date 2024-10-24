0858bd7f601995cc329317f5c6204be2a986b44b
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-24 02:58:21 -0700

    updated task controller to include getusertasks

--- server/dist/src/controllers/taskController.js
+++ server/dist/src/controllers/taskController.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateTaskStatus = exports.createTask = exports.getTasks = void 0;
+exports.getUserTasks = exports.updateTaskStatus = exports.createTask = exports.getTasks = void 0;
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const getTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -24,12 +24,14 @@ const getTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                 assignee: true,
                 comments: true,
                 attachments: true,
-            }
+            },
         });
         res.json(tasks);
     }
     catch (error) {
-        res.status(500).json({ message: `Error retrieving tasks: ${error.message}` });
+        res
+            .status(500)
+            .json({ message: `Error retrieving tasks: ${error.message}` });
     }
 });
 exports.getTasks = getTasks;
@@ -49,12 +51,14 @@ const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
                 projectId,
                 authorUserId,
                 assignedUserId,
-            }
+            },
         });
         res.status(201).json(newTask);
     }
     catch (error) {
-        res.status(500).json({ message: `Error creating task: ${error.message}` });
+        res
+            .status(500)
+            .json({ message: `Error creating a task: ${error.message}` });
     }
 });
 exports.createTask = createTask;
@@ -68,7 +72,7 @@ const updateTaskStatus = (req, res) => __awaiter(void 0, void 0, void 0, functio
             },
             data: {
                 status: status,
-            }
+            },
         });
         res.json(updatedTask);
     }
@@ -77,3 +81,27 @@ const updateTaskStatus = (req, res) => __awaiter(void 0, void 0, void 0, functio
     }
 });
 exports.updateTaskStatus = updateTaskStatus;
+const getUserTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    try {
+        const tasks = yield prisma.task.findMany({
+            where: {
+                OR: [
+                    { authorUserId: Number(userId) },
+                    { assignedUserId: Number(userId) },
+                ],
+            },
+            include: {
+                author: true,
+                assignee: true,
+            },
+        });
+        res.json(tasks);
+    }
+    catch (error) {
+        res
+            .status(500)
+            .json({ message: `Error retrieving user's tasks: ${error.message}` });
+    }
+});
+exports.getUserTasks = getUserTasks;
