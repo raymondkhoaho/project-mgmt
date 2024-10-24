51e235ac10f01c7a22a5f150b7623a80c4a9e692
Raymond Ho <raymondkhoaho@gmail.com>
2024-10-24 02:56:43 -0700

    home page completed

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import {
+  Priority,
+  Project,
+  Task,
   useGetProjectsQuery,
   useGetTasksQuery,
-  Priority,
-  Task,
-  Project,
 } from "@/state/api";
 import React from "react";
 import { useAppSelector } from "../redux";
-import { GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Header from "@/components/Header";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
 
 const taskColumns: GridColDef[] = [
   { field: "title", headerName: "Title", width: 200 },
@@ -77,7 +92,72 @@ const HomePage = () => {
         text: "#000000",
       };
 
-  return <div>HomePage</div>;
+  return (
+    <div className="container h-full w-[100%] bg-gray-100 bg-transparent p-8">
+      <Header name="Project Management Dashboard" />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="rounded-lg bg-white p-4 shadow dark:bg-dark-secondary">
+          <h3 className="mb-4 text-lg font-semibold dark:text-white">
+            Task Priority Distribution
+          </h3>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={taskDistribution}>
+              <CartesianGrid
+                strokeDasharray="3 3"
+                stroke={chartColors.barGrid}
+              />
+              <XAxis dataKey="name" stroke={chartColors.text} />
+              <YAxis stroke={chartColors.text} />
+              <Tooltip
+                contentStyle={{
+                  width: "min-content",
+                  height: "min-content",
+                }}
+              />
+              <Legend />
+              <Bar dataKey="count" fill={chartColors.bar} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="rounded-lg bg-white p-4 shadow dark:bg-dark-secondary">
+          <h3 className="mb-4 text-lg font-semibold dark:text-white">
+            Project Status
+          </h3>
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+              <Pie dataKey="count" data={projectStatus} fill="#82ca9d" label>
+                {projectStatus.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="rounded-lg bg-white p-4 shadow dark:bg-dark-secondary md:col-span-2">
+          <h3 className="mb-4 text-lg font-semibold dark:text-white">
+            Your Tasks
+          </h3>
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={tasks}
+              columns={taskColumns}
+              checkboxSelection
+              loading={tasksLoading}
+              getRowClassName={() => "data-grid-row"}
+              getCellClassName={() => "data-grid-cell"}
+              className={dataGridClassNames}
+              sx={dataGridSxStyles(isDarkMode)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
